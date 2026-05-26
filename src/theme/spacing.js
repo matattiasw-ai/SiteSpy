bba779181a8ac1e44ba0c819ef955bf0c25d264c
Johannes M Kandjeke <Kandjekejohannes54@gmail.com>
2026-05-26 10:15:00 +0200

    Define global spacing and typography constants

---
+++ src/theme/spacing.js
@@ -0,0 +1,13 @@
+export const spacing = {
+  xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 18,
+  xl: 28,
+  xxl: 42,
+  screen: 20,
+  inputHeight: 52,
+  buttonHeight: 52,
+  cardRadius: 8,
+  controlRadius: 8
+};
