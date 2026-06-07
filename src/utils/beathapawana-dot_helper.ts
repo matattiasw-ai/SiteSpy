4176cb837994cd6566cb5ff85b49f822b167dfd1
Haipumbu Beatha NP <beathahaipumbu1@gmail.com>
2026-06-07 15:42:54 +0200

    Optimize image picker handling

--- src/utils/beathapawana-dot_helper.ts
+++ src/utils/beathapawana-dot_helper.ts
@@ -4,3 +4,5 @@ export const helper_beathapawana-dot = () => {
 };
 // Log point 1: Remove redundant imports and optimize build size at 2026-06-05T12:35:43+02:00
 console.log("Remove redundant imports and optimize build size");
+// Log point 2: Optimize image picker handling at 2026-06-07T15:42:54+02:00
+console.log("Optimize image picker handling");
